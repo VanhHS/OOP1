0357c9182bcc60753352850c1db1060296f52e0c
VanhHS <114225530+VanhHS@users.noreply.github.com>
2023-04-12 13:37:26 +0700

    Add files via upload

--- baithuchanh/src/sudungtoantu.java
+++ baithuchanh/src/sudungtoantu.java
@@ -6,12 +6,11 @@ public class sudungtoantu {
         float height;//khai bao bien chieu dai
         Scanner scanner = new Scanner(System.in);
             //quet du lieu nhap vao
-        System.out.println("Enter width: ");//nhap do dai chieu rong
-        width = scanner.nextFloat();//quet du lieu nhap vao
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();//Khoi tao gia tri cho bien width va nhap gia tri cua bien width
 
-        System.out.println("Enter height: ");// nhap do dai chieu dai
-        height = scanner.nextFloat();//quet du lieu nhap vao
-
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();//Khoi tao gia tri cho bien height va nhap gia tri cua bien height
         float area = width * height ;
         System.out.println("Area is =" + area);//su dung toan tu nhan tinh dien tich hcn
     }
