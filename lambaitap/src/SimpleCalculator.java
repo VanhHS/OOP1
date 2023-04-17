e3c8cf9373fe23cab812a112fa11c71481c3a6cc
VanhHS <114225530+VanhHS@users.noreply.github.com>
2023-04-17 11:05:25 +0700

    Add files via upload

--- lambaitap/src/SimpleCalculator.java
+++ lambaitap/src/SimpleCalculator.java
@@ -2,18 +2,38 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);//Khai bao bien Scanner
         int num1, num2;//Khai bao 2 bien num1, num2
-        Scanner sc = new Scanner(System.in);//Khai bao bien Scanner
+
 
         System.out.println("Nhập số thứ nhất: ");
-        num1 = sc.nextInt();//Gan gia tri cho bien num1
+        num1 = input.nextInt();//Nhap va gan gia tri cho bien num1
         System.out.println("Nhập so thu hai: ");
-        num2 = sc.nextInt();//Gan gia tri cho bien num2
+        num2 = input.nextInt();//NHap va gan gia tri cho bien num2
 
-        System.out.println("Tổng hai số là: " + (num1 + num2));
-        System.out.println("Tích hai số là: " + (num1 * num2));
-        System.out.println("Thương hai số là: " + (num1 / num2));
-        System.out.println("Phần dư khi chia hai số là: " + (num1 % num2));
-        //In ra ket qua
+        System.out.print("Nhap phep tinh (+,-,*,/,%)");
+        char pheptinh = input.next().charAt(0);//Nhap va gan gia chi cho pheptinh
+
+        switch (pheptinh) {
+            case '+':
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println(num1 + "*" + num2 + " = " + (num1 * num2));
+                break;
+            case '/':
+                System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                break;
+            case '%':
+                System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+                break;
+
+            default:
+                System.out.println("Phep tinh khong hop le.");
+                break;
+        }
     }
 }
