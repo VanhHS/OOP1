05652afc33a8a3f5a1d6e83870ad26ca118e1215
VanhHS <114225530+VanhHS@users.noreply.github.com>
2023-05-09 16:13:37 +0700

    Add files via upload

---
+++ thuchanh/src/timGiaTriBeNhat.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class timGiaTriBeNhat {
+    public static void main(String[] args) {
+        int size;
+        int []array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size: ");
+            size = scanner.nextInt();
+            if (size > 20)
+                //Khai báo biến và nhập kiểm tra kích thước mảng
+                System.out.println("Size shouldn't exceed 20");
+        }while (size > 20);
+        array  = new int[size];
+        int i = 0;
+        while (i< array.length){
+            System.out.print("Enter element" + (i +1) + " : ");
+            array[i]= scanner.nextInt();
+            i++;
+            //Nhập giá trị cho các phần tử của mảng
+        }
+        System.out.print("Propety list: ");
+        for (int j =0; j < array.length; j++){
+            System.out.println(array[j] + "\t");
+            //in ra danh sách đã nhập
+        }
+        int min = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++){
+            if (array[j] < min){
+                min = array[j];
+                index = j +1;
+                //Duyệ các phần tử trong mảng để tìm giá trị bé nhất
+            }
+        }
+        System.out.println("The largest property value in the list is " + min + " ,at position " + index);
+    }
+}
